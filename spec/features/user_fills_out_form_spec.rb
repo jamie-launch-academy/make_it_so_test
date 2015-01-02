d64993a18dfde5e01fe66727e96a8716a3e92635
Jamie Francese <jamienf@gmail.com>
2015-01-01 21:06:09 -0500

    initial commit

---
+++ spec/features/user_fills_out_form_spec.rb
@@ -0,0 +1,15 @@
+require 'rails/helper'
+
+feature "user fills out a form", %Q{
+
+  As a user
+  I want to fill out a form
+  So that I may test my product
+
+  Acceptance Criteria
+  [ ] I must only be able to fill out a form when I am signed in
+  [ ] I must be able to
+  
+
+
+} do
